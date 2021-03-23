a4e1204bf712980651bec9ba3acd6dfcafe70ead
sandypockets <zac@znacreative.com>
2021-03-23 14:17:11 -0400

    Remove unused counter variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,8 @@
 // Track num of characters remaining for new tweet
 $(document).ready(function() {
   console.log( "ready!" );
-  let counter = 0;
   $("#tweet-text").on('keypress', function() {
     console.log(this);
-    counter++
     $("#tweet-text").on('keyup', function() {
       // Count number of characters in input field
       let currentCount = $("#tweet-text").val().length;
