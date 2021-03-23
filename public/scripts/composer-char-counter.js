4f5e75c53578f18c6e0b56e03a149b822315af73
sandypockets <zac@znacreative.com>
2021-03-22 21:24:04 -0400

    Start to add event handlers for tweet character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready()
+
+
+// Implementing event handler to track characters typed into
+// new tweets form. Will then adjust the num of characters remaining.
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Test that it's working!");
+});
