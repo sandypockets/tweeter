e945dfd2baac59b089a050931f59cb3ab91cfdc7
sandypockets <zac@znacreative.com>
2021-03-23 13:59:05 -0400

    Fix character count starting at zero

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
   $("#tweet-text").on('keypress', function() {
     console.log(this); //The this keyword is a reference to the button
     counter++
-    let currentCount = $("#tweet-text").val().length;
-    
-    //let maxChars = 140 - currentCount;
+
+    // Count number of characters in input field
+    let currentCount = $("#tweet-text").val().length + 1;
     console.log("CurrentCount:", currentCount);
+
+    //let maxChars = 140 - currentCount;
+
+    // Characters left
     let newCharCountVal = 140 - counter;
 
     // Adjust character counter based on counter var
