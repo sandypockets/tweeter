aa538207bb7199b4fb9a4532f1a143a51df85f32
sandypockets <zac@znacreative.com>
2021-03-23 13:55:48 -0400

    Begin implementing character counter with jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,20 @@
 // new tweets form. Will then adjust the num of characters remaining.
 $(document).ready(function() {
   console.log( "ready!" );
-});
+  let counter = 0;
+  $("#tweet-text").on('keypress', function() {
+    console.log(this); //The this keyword is a reference to the button
+    counter++
+    let currentCount = $("#tweet-text").val().length;
+    
+    //let maxChars = 140 - currentCount;
+    console.log("CurrentCount:", currentCount);
+    let newCharCountVal = 140 - counter;
 
+    // Adjust character counter based on counter var
+    $(".counter").text(newCharCountVal);
+    if (newCharCountVal <= 0) {
+      $(".counter").css("color", "red");
+    }
+  });
+});
