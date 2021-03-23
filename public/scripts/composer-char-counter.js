cf2c8c2c93430631dc45f5bacb7bb2e47183b52f
sandypockets <zac@znacreative.com>
2021-03-23 14:13:43 -0400

    Fix character counter for copy/paste text

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,19 +7,25 @@ $(document).ready(function() {
     console.log(this); //The this keyword is a reference to the button
     counter++
 
-    // Count number of characters in input field
-    let currentCount = $("#tweet-text").val().length + 1;
-    console.log("CurrentCount:", currentCount);
+    $("#tweet-text").on('keyup', function() {
+      // Count number of characters in input field
+      let currentCount = $("#tweet-text").val().length;
+      console.log("CurrentCount:", currentCount);
+      let newCharCountVal = 140 - currentCount;
+      $(".counter").text(newCharCountVal);
+      if (newCharCountVal <= 0) {
+        $(".counter").css("color", "red");
+      } else {
+        $(".counter").css("color", "black");
+      }
+    })
 
     //let maxChars = 140 - currentCount;
 
     // Characters left
-    let newCharCountVal = 140 - counter;
+    //let newCharCountVal = 140 - counter;
 
     // Adjust character counter based on counter var
-    $(".counter").text(newCharCountVal);
-    if (newCharCountVal <= 0) {
-      $(".counter").css("color", "red");
-    }
+
   });
 });
