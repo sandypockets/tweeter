0d8ffadb3cf53bdc54a92e5f6bb698cb36980ed7
sandypockets <zac@znacreative.com>
2021-03-23 13:00:15 -0400

    Fix jQuery implementation

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,6 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-
-
 // Implementing event handler to track characters typed into
 // new tweets form. Will then adjust the num of characters remaining.
-/* $(document).ready(function() {
-  // --- our code goes here ---
-  console.log("Test that it's working!");
-}); */
-
-
-// A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function() {
   console.log( "ready!" );
 });
 
