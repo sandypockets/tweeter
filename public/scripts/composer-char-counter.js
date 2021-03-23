12810ab1c3052f6364cd524afb89faccc856d7e5
sandypockets <zac@znacreative.com>
2021-03-23 14:15:58 -0400

    Rename variables for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,20 @@
-// Implementing event handler to track characters typed into
-// new tweets form. Will then adjust the num of characters remaining.
+// Track num of characters remaining for new tweet
 $(document).ready(function() {
   console.log( "ready!" );
   let counter = 0;
   $("#tweet-text").on('keypress', function() {
-    console.log(this); //The this keyword is a reference to the button
+    console.log(this);
     counter++
-
     $("#tweet-text").on('keyup', function() {
       // Count number of characters in input field
       let currentCount = $("#tweet-text").val().length;
-      console.log("CurrentCount:", currentCount);
-      let newCharCountVal = 140 - currentCount;
-      $(".counter").text(newCharCountVal);
-      if (newCharCountVal <= 0) {
+      let charsRemaining = 140 - currentCount;
+      $(".counter").text(charsRemaining);
+      if (charsRemaining <= 0) {
         $(".counter").css("color", "red");
       } else {
         $(".counter").css("color", "black");
       }
     })
-
-    //let maxChars = 140 - currentCount;
-
-    // Characters left
-    //let newCharCountVal = 140 - counter;
-
-    // Adjust character counter based on counter var
-
   });
 });
