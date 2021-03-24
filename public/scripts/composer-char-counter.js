22af7baca3e41deceeca35110e82b3ec56a053bb
sandypockets <zac@znacreative.com>
2021-03-23 20:05:05 -0400

    Clean up memory leak, change let vars to const

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,15 @@
 // Track num of characters remaining for new tweet
 $(document).ready(function() {
   console.log( "ready!" );
-  $("#tweet-text").on('keypress', function() {
-    console.log(this);
-    $("#tweet-text").on('keyup', function() {
-      // Count number of characters in input field
-      let currentCount = $("#tweet-text").val().length;
-      let charsRemaining = 140 - currentCount;
-      $(".counter").text(charsRemaining);
-      if (charsRemaining <= 0) {
-        $(".counter").css("color", "red");
-      } else {
-        $(".counter").css("color", "black");
-      }
-    })
-  });
+  $("#tweet-text").on('keyup', function() {
+    // Count number of characters in input field
+    const currentCount = $("#tweet-text").val().length;
+    const charsRemaining = 140 - currentCount;
+    $(".counter").text(charsRemaining);
+    if (charsRemaining < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black");
+    }
+  })
 });
